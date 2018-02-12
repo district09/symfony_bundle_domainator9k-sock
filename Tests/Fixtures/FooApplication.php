e46ce8009997e908a7cffca365ecd5a9b874fc4b
Pieter MAssoels <massoelspieter@gmail.com>
2018-02-12 14:02:56 +0100

    WEBDOM-292 : application type propery update

--- Tests/Fixtures/FooApplication.php
+++ Tests/Fixtures/FooApplication.php
@@ -8,9 +8,14 @@ use DigipolisGent\Domainator9k\CoreBundle\Entity\AbstractApplication;
 class FooApplication extends AbstractApplication
 {
 
-    public static function getType()
+    public static function getApplicationType(): string
     {
         return 'foo';
+    }
+
+    public static function getFormType(): string
+    {
+        return 'random';
     }
 
     public function setId($id)
