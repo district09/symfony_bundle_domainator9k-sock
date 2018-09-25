7b5b7b8ecaf7dcb9f051eb607ac641d3fd10e760
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-09-25 10:13:53 +0200

    WEBDOM-404: Added test for sock alias field type.

--- Tests/FieldType/SockAliasesFieldTypeTest.php
+++ Tests/FieldType/SockAliasesFieldTypeTest.php
@@ -3,143 +3,53 @@
 
 namespace DigipolisGent\Domainator9k\SockBundle\Tests\FieldType;
 
-use DigipolisGent\Domainator9k\CoreBundle\Entity\ApplicationEnvironment;
-use DigipolisGent\Domainator9k\SockBundle\FieldType\SshKeyChoiceFieldType;
-use DigipolisGent\Domainator9k\SockBundle\Service\ApiService;
+use DigipolisGent\Domainator9k\SockBundle\FieldType\SockAliasesFieldType;
 use DigipolisGent\Domainator9k\SockBundle\Tests\Fixtures\FooApplication;
-use DigipolisGent\SettingBundle\Service\DataValueService;
-use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
-class SshKeyChoiceFieldTypeTest extends TestCase
+class SockAliasesFieldTypeTest extends TestCase
 {
 
     public function testGetName()
     {
-        $this->assertEquals('ssh_key_choice', SshKeyChoiceFieldType::getName());
+        $this->assertEquals('sock_aliases', SockAliasesFieldType::getName());
     }
 
     public function testGetFormType()
     {
-        $apiService = $this->getApiServiceMock();
-        $dataValueService = $this->getDataValueServiceMock();
-        $fieldType = new SshKeyChoiceFieldType($apiService, $dataValueService);
-        $this->assertEquals(ChoiceType::class, $fieldType->getFormType());
+        $fieldType = new SockAliasesFieldType();
+        $this->assertEquals(CollectionType::class, $fieldType->getFormType());
     }
 
     public function testGetOptionsForApplication()
     {
-        $apiService = $this->getApiServiceMock();
-        $dataValueService = $this->getDataValueServiceMock();
-
         $originEntity = new FooApplication();
 
-        $value = '[1,2]';
+        $value = '["example.com","example.be"]';
 
-        $sshKeys = [];
-
-        for ($i = 0; $i < 10; $i++) {
-            $sshKeys[] = [
-                'id' => $i,
-                'description' => 'Key ' . $i
-            ];
-        }
-
-        $apiService
-            ->expects($this->at(0))
-            ->method('getSshKeys')
-            ->willReturn($sshKeys);
-
-        $fieldType = new SshKeyChoiceFieldType($apiService, $dataValueService);
+        $fieldType = new SockAliasesFieldType();
         $fieldType->setOriginEntity($originEntity);
         $options = $fieldType->getOptions($value);
 
-        $this->assertArrayHasKey('multiple', $options);
-        $this->assertArrayHasKey('expanded', $options);
-        $this->assertArrayHasKey('choices', $options);
-        $this->assertArrayHasKey('data', $options);
-
-        $this->assertCount(2, $options['data']);
-        $this->assertCount(10, $options['choices']);
-    }
-
-
-    public function testGetOptionsForApplicationEnvironment()
-    {
-        $apiService = $this->getApiServiceMock();
-        $dataValueService = $this->getDataValueServiceMock();
-
-        $originEntity = new ApplicationEnvironment();
-
-        $value = '[1,2]';
-
-        $sshKeys = [];
-
-        for ($i = 0; $i < 10; $i++) {
-            $sshKeys[] = [
-                'id' => $i,
-                'description' => 'Key ' . $i
-            ];
-        }
-
-        $apiService
-            ->expects($this->at(0))
-            ->method('getSshKeys')
-            ->willReturn($sshKeys);
-
-        $dataValueService
-            ->expects($this->at(0))
-            ->method('getValue')
-            ->willReturn(['1','5','6','8']);
-
-        $fieldType = new SshKeyChoiceFieldType($apiService, $dataValueService);
-        $fieldType->setOriginEntity($originEntity);
-        $options = $fieldType->getOptions($value);
-
-        $this->assertArrayHasKey('multiple', $options);
-        $this->assertArrayHasKey('expanded', $options);
-        $this->assertArrayHasKey('choices', $options);
-        $this->assertArrayHasKey('data', $options);
-
-        $this->assertCount(4, $options['data']);
-        $this->assertCount(10, $options['choices']);
+        $this->assertEquals($options['entry_type'], UrlType::class);
+        $this->assertEquals($options['allow_add'], true);
+        $this->assertEquals($options['allow_delete'], true);
+        $this->assertEquals($options['delete_empty'], true);
+        $this->assertEquals($options['data'], json_decode($value, true));
     }
 
 
     public function testEncodeValue()
     {
-        $apiService = $this->getApiServiceMock();
-        $dataValueService = $this->getDataValueServiceMock();
-        $fieldType = new SshKeyChoiceFieldType($apiService, $dataValueService);
-        $this->assertEquals('["two"]', $fieldType->encodeValue(['one' => 'two']));
+        $fieldType = new SockAliasesFieldType();
+        $this->assertEquals('["example.com","example.be"]', $fieldType->encodeValue(["example.com", "example.be"]));
     }
 
     public function testDecodeValue()
     {
-        $apiService = $this->getApiServiceMock();
-        $dataValueService = $this->getDataValueServiceMock();
-        $fieldType = new SshKeyChoiceFieldType($apiService, $dataValueService);
-        $this->assertEquals(['one' => 'two'], $fieldType->decodeValue('{"one":"two"}'));
-    }
-
-    private function getApiServiceMock()
-    {
-        $mock = $this
-            ->getMockBuilder(ApiService::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        return $mock;
-    }
-
-    private function getDataValueServiceMock()
-    {
-        $mock = $this
-            ->getMockBuilder(DataValueService::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        return $mock;
+        $fieldType = new SockAliasesFieldType();
+        $this->assertEquals(["example.com", "example.be"], $fieldType->decodeValue('["example.com","example.be"]'));
     }
 }
