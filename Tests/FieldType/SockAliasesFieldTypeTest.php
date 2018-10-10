bd3472eeeba7fb96846c8c0891f63b4eb013f9b0
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-10-10 09:16:07 +0200

    Fix sock aliases field tests.

--- Tests/FieldType/SockAliasesFieldTypeTest.php
+++ Tests/FieldType/SockAliasesFieldTypeTest.php
@@ -7,7 +7,7 @@ use DigipolisGent\Domainator9k\SockBundle\FieldType\SockAliasesFieldType;
 use DigipolisGent\Domainator9k\SockBundle\Tests\Fixtures\FooApplication;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class SockAliasesFieldTypeTest extends TestCase
 {
@@ -33,7 +33,7 @@ class SockAliasesFieldTypeTest extends TestCase
         $fieldType->setOriginEntity($originEntity);
         $options = $fieldType->getOptions($value);
 
-        $this->assertEquals($options['entry_type'], UrlType::class);
+        $this->assertEquals($options['entry_type'], TextType::class);
         $this->assertEquals($options['allow_add'], true);
         $this->assertEquals($options['allow_delete'], true);
         $this->assertEquals($options['delete_empty'], true);
